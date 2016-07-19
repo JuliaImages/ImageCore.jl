c98c5dbddd374e6334301fc16e8d0fc09c823c2f
Tim Holy <tim.holy@gmail.com>
2016-07-19 14:33:38 -0500

    Add benchmarks for ColorView

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -1,30 +1,30 @@
-using ImagesCore, Colors, Base.Test
-using BenchmarkTools, JLD
+using ImagesCore, Colors, ColorVectorSpace
+using Base.Test, BenchmarkTools, JLD
 
 # Different access patterns (getindex)
 function mysum_elt_boundscheck(A)
-    s = 0.0
+    s = zero(eltype(A))
     for a in A
         s += a
     end
     s
 end
 function mysum_index_boundscheck(A)
-    s = 0.0
+    s = zero(eltype(A))
     for I in eachindex(A)
         s += A[I]
     end
     s
 end
 function mysum_elt_inbounds(A)
-    s = 0.0
+    s = zero(eltype(A))
     @inbounds for a in A
         s += a
     end
     s
 end
 function mysum_index_inbounds_simd(A)
-    s = 0.0
+    s = zero(eltype(A))
     @inbounds @simd for I in eachindex(A)
         s += A[I]
     end
@@ -51,17 +51,28 @@ BenchmarkTools.DEFAULT_PARAMETERS.seconds = 0.1
 ssz = (100,100)
 a = rand(3,ssz...)
 c = reinterpret(RGB{Float64}, a, ssz)
-v = ChannelView(c)
+vchan = ChannelView(c)
+vcol = ColorView{RGB}(a)
 suite["ChannelView"] = BenchmarkGroup()
 channelviewfuncs = (mysum_elt_boundscheck,
                     mysum_index_boundscheck,
                     mysum_elt_inbounds,
                     mysum_index_inbounds_simd)
-cvtol = Dict(mysum_index_inbounds_simd=>20)  # @simd doesn't work for ChannelView :(
-cvdefault = 10
+chanvtol = Dict(mysum_index_inbounds_simd=>20)  # @simd doesn't work for ChannelView :(
+chanvdefault = 10
+colvtol = Dict(mysum_elt_boundscheck=>5, mysum_index_boundscheck=>5)
+colvdefault = 3
+
 for f in channelviewfuncs
-    for x in (a, v)
+    for x in (a, vchan)
         suite["ChannelView"][string(f), string(typeof(x).name.name)] = @benchmarkable $(f)($x)
+    end
+end
+
+suite["ColorView"] = BenchmarkGroup()
+for f in channelviewfuncs
+    for x in (c, vcol)
+        suite["ColorView"][string(f), string(typeof(x).name.name)] = @benchmarkable $(f)($x)
     end
 end
 
@@ -71,10 +82,17 @@ end
 loadparams!(suite, load("params.jld", "suite"), :evals, :samples, :seconds)
 results = run(suite)
 
-cvr = results["ChannelView"]
+chanvr = results["ChannelView"]
 for f in channelviewfuncs
-    cv = cvr[string(f), "ChannelView"]
-    a =  cvr[string(f), "Array"]
-    tol = haskey(cvtol, f) ? cvtol[f] : cvdefault
+    cv = chanvr[string(f), "ChannelView"]
+    a =  chanvr[string(f), "Array"]
+    tol = haskey(chanvtol, f) ? chanvtol[f] : chanvdefault
+    @test time(median(cv)) < tol*time(median(a))
+end
+colvr = results["ColorView"]
+for f in channelviewfuncs
+    cv = colvr[string(f), "ColorView"]
+    a =  colvr[string(f), "Array"]
+    tol = haskey(colvtol, f) ? colvtol[f] : colvdefault
     @test time(median(cv)) < tol*time(median(a))
 end

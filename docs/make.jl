118502e66812e9a5ab8f21223eb24b14d9db5a32
Tim Holy <tim.holy@gmail.com>
2016-09-12 13:44:06 -0500

    Switch to Documenter's own HTML formatting

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,9 @@
 using Documenter, ImageCore
 
-makedocs(modules = [ImageCore])
+makedocs(modules  = [ImageCore],
+         format   = Documenter.Formats.HTML,
+         sitename = "ImageCore",
+         pages    = ["index.md", "views.md", "traits.md", "reference.md"])
 
-deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-           repo   = "github.com/JuliaImages/ImageCore.jl.git")
+deploydocs(repo   = "github.com/JuliaImages/ImageCore.jl.git")
+#           deps   = Deps.pip("mkdocs", "python-markdown-math"))

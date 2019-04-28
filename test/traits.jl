579891d4d1dbc83907329b2191e4d8db11703e13
Johnny Chen <johnnychen94@hotmail.com>
2019-04-28 09:18:52 +0000

    Add NumberLike and GrayImageLike traits
    Currently these kinds of traits are used throughout JuliaImages
    ecosystem, adding this to ImageCore improves the consistency
    
    These traits should not be exported to avoid potential name conficts

--- test/traits.jl
+++ test/traits.jl
@@ -1,5 +1,7 @@
 using ImageCore, Colors, FixedPointNumbers, ColorVectorSpace, MappedArrays, OffsetArrays
 using Test
+using ImageCore: NumberLike, RealLike, FloatLike, FractionalLike, IntegerLike,
+      GrayLike, GrayImageLike
 
 @testset "Image traits" begin
     for (B, swap) in ((rand(UInt16(1):UInt16(20), 3, 5), false),
@@ -30,6 +32,104 @@ using Test
         assert_timedim_last(B)
         @test width(B) == 5
         @test height(B) == 3
+    end
+end
+
+@testset "*Like traits" begin
+    # delibrately written in a redundant way
+    @testset "NumberLike" begin
+        @test RealLike <: NumberLike
+        @test FloatLike <: NumberLike
+        @test FractionalLike <: NumberLike
+        @test IntegerLike <: NumberLike
+
+        @test Number <: NumberLike
+        @test Real <: NumberLike
+        @test AbstractFloat <: NumberLike
+        @test FixedPoint <: NumberLike
+        @test Integer <: NumberLike
+        @test Bool <: NumberLike
+
+        @test Gray <: NumberLike
+        @test Gray{<:AbstractFloat} <: NumberLike
+        @test Gray{<:Bool} <: NumberLike
+
+        @test isa(oneunit(Gray), NumberLike)
+    end
+
+    @testset "RealLike" begin
+        @test FloatLike <: RealLike
+        @test FractionalLike <: RealLike
+        @test IntegerLike <: RealLike
+
+        @test Real <: RealLike
+        @test AbstractFloat <: RealLike
+        @test FixedPoint <: RealLike
+        @test Integer <: RealLike
+        @test Bool <: RealLike
+
+        @test Gray{<:AbstractFloat} <: RealLike
+        @test Gray{<:Bool} <: RealLike
+        @test Gray{<:FixedPoint} <: RealLike
+
+        @test isa(oneunit(Gray), RealLike)
+    end
+
+    @testset "FractionalLike" begin
+        @test AbstractFloat <: FractionalLike
+        @test FixedPoint <: FractionalLike
+
+        @test !(Gray <: FractionalLike)
+        @test Gray{<:AbstractFloat} <: FractionalLike
+        @test Gray{<:FixedPoint} <: FractionalLike
+
+        @test isa(oneunit(Gray), FractionalLike)
+    end
+
+    @testset "GrayLike" begin
+        @test AbstractFloat <: GrayLike
+        @test FixedPoint <: GrayLike
+        @test Bool <: GrayLike
+
+        @test Gray <: GrayLike
+        @test Gray{<:AbstractFloat} <: GrayLike
+        @test Gray{<:FixedPoint} <: GrayLike
+        @test Gray{Bool} <: GrayLike
+
+        @test isa(oneunit(Gray), GrayLike)
+    end
+
+    @testset "FloatLike" begin
+        @test AbstractFloat <: FloatLike
+
+        @test !(Gray <: FloatLike)
+        @test Gray{<:AbstractFloat} <: FloatLike
+
+        @test !isa(oneunit(Gray), FloatLike)
+    end
+
+    @testset "IntegerLike" begin
+        @test Integer <: IntegerLike
+        @test FixedPoint <: IntegerLike
+        @test Bool <: IntegerLike
+
+        @test !(Gray <: IntegerLike)
+        @test Gray{<:Bool} <: IntegerLike
+        @test Gray{<:FixedPoint} <: IntegerLike
+
+        @test isa(oneunit(Gray), IntegerLike)
+    end
+
+    @testset "GrayImageLike" begin
+        sz = (3,3)
+        @test isa(rand(Bool, sz), GrayImageLike)
+        @test isa(rand(N0f8, sz), GrayImageLike)
+        @test isa(rand(Float32, sz), GrayImageLike)
+
+        @test isa(rand(Gray, sz), GrayImageLike)
+        @test isa(rand(Gray{Bool}, sz), GrayImageLike)
+        @test isa(rand(Gray{N0f8}, sz), GrayImageLike)
+        @test isa(rand(Gray{Float32}, sz), GrayImageLike)
     end
 end
 

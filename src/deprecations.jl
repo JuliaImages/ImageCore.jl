32a1bc97fd6ecdac69c5165a13279c1a3f231cf6
Tim Holy <tim.holy@gmail.com>
2020-09-10 06:03:25 -0500

    Force the deprecation warnings (#140)
    On Julia 1.5 and higher, depwarns are off by default.
    This forces them to be displayed once.

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,3 +1,21 @@
+const warned_once = Ref(false)
+if VERSION >= v"1.5"
+    function forced_depwarn(msg, sym)
+        opts = Base.JLOptions()
+        if !warned_once[] && !(opts.depwarn == 1)
+            @warn msg
+            @info """It is recommended that you fix this now to avoid breakage when a new version is released and this warning is removed.
+                     Tip: to see all deprecation warnings together with code locations, launch Julia with `--depwarn=yes` and rerun your code."""
+            warned_once[] = true
+        else
+            Base.depwarn(msg, sym)
+        end
+        return nothing
+    end
+else
+    forced_depwarn(msg, sym) = Base.depwarn(msg, sym)
+end
+
 Base.@deprecate_binding ChannelView channelview
 
 export ColorView
@@ -9,13 +27,13 @@ struct ColorView{C<:Colorant,N,A<:AbstractArray} <: AbstractArray{C,N}
         n = length(colorview_size(C, parent))
         n == N || throw(DimensionMismatch("for an $N-dimensional ColorView with color type $C, input dimensionality should be $n instead of $(ndims(parent))"))
         checkdim1(C, axes(parent))
-        Base.depwarn("ColorView{C}(A) is deprecated, use colorview(C, A)", :ColorView)
+        forced_depwarn("ColorView{C}(A) is deprecated, use colorview(C, A)", :ColorView)
         colorview(C, A)
     end
 end
 
 function ColorView{C}(A::AbstractArray) where C<:Colorant
-    Base.depwarn("ColorView{C}(A) is deprecated, use colorview(C, A)", :ColorView)
+    forced_depwarn("ColorView{C}(A) is deprecated, use colorview(C, A)", :ColorView)
     colorview(C, A)
 end
 
@@ -33,13 +51,14 @@ end
 
 const _explain =
 """will soon switch to returning an array with non-concrete element type, which adds flexibility but
-   with great cost to performance. To maintain the current behavior, use """
+   with great cost to performance. To maintain the current behavior, use"""
+
 function convert(::Type{Array{Cdest}}, img::AbstractArray{Csrc,n}) where {Cdest<:Colorant,n,Csrc<:Colorant}
     if isconcretetype(Cdest)
         # This mimics the Base implementation
         return img isa Array{Cdest} ? img : Array{Cdest}(img)
     end
-    Base.depwarn("`convert(Array{$(cname(Cdest))}, img)` $_explain $(cname(Cdest)).(img) instead", :convert)
+    forced_depwarn("`convert(Array{$(cname(Cdest))}, img)` $_explain `$(cname(Cdest)).(img)` instead.", :convert)
     convert(Array, Cdest.(img))
 end
 
@@ -47,7 +66,7 @@ function convert(::Type{Array{Cdest}}, img::AbstractArray{T,n}) where {Cdest<:Co
     if isconcretetype(Cdest)
         return img isa Array{Cdest} ? img : Array{Cdest}(img)
     end
-    Base.depwarn("`convert(Array{$(cname(Cdest))}, img)` $_explain $(cname(Cdest)).(img) instead", :convert)
+    forced_depwarn("`convert(Array{$(cname(Cdest))}, img)` $_explain `$(cname(Cdest)).(img)` instead.", :convert)
     convert(Array, Cdest.(img))
 end
 
@@ -56,9 +75,9 @@ function convert(::Type{OffsetArray{Cdest,n,A}}, img::AbstractArray{Csrc,n}) whe
         return img isa OffsetArray{Cdest,n,A} ? img : (img isa OffsetArray ? OffsetArray(A(Cdest.(parent(img))), axes(img)) : OffsetArray(A(Cdest.(img)), axes(img)))
     end
     if img isa OffsetArray
-        Base.depwarn("`convert(OffsetArray{$(cname(Cdest))}, img)` $_explain $(cname(Cdest)).(img) instead", :convert)
+        forced_depwarn("`convert(OffsetArray{$(cname(Cdest))}, img)` $_explain `$(cname(Cdest)).(img)` instead.", :convert)
     else
-        Base.depwarn("`convert(OffsetArray{$(cname(Cdest))}, img)` $_explain OffsetArray($(cname(Cdest)).(img)) instead", :convert)
+        forced_depwarn("`convert(OffsetArray{$(cname(Cdest))}, img)` $_explain `OffsetArray($(cname(Cdest)).(img), axes(img))` instead.", :convert)
     end
     OffsetArray(Cdest.(img), axes(img))
 end
